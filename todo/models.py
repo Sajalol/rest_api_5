d83f4fbd5093f499eca267a64976ade963e66e3e
Stian Lystad <Stianlystad_1@hotmail.com>
2023-01-06 18:08:15 +0000

    added models to todo

--- todo/models.py
+++ todo/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+import datetime
 # Create your models here.
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
+    content = models.TextField(blank=True)
+    images = models.ImageField(upload_to='images/', blank=True)
+    due_date_time = models.DateField(default=datetime.date.today)
     completed = models.BooleanField(default=False, blank=True,       null=True)
     objects = models.Manager()
     def __str__(self):
--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import TaskSerializer
 from .models import Task
+from profiles import views
+from profiles import serializers
+from django_api.permissions import IsOwnerOrReadOnly
+
+
 """
 API Overview
 """
@@ -16,8 +21,48 @@ def apiOverview(request):
     }
     return Response(api_urls)
 
+"Get "
+
 @api_view(['GET'])
 def taskList(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many = True)
     return Response(serializer.data)
+
+"""
+This Function going to display Detailed view of one perticuler task with the help of pk.
+"""
+@api_view(['GET'])
+def taskDetail(request, pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many = False)
+    return Response(serializer.data)
+
+"Update single post"
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+    task = Task.objects.get(id = pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+"Creating new posts"
+
+@api_view(['POST'])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+"Delete posts"
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+    task = Task.objects.get(id = pk)
+    task.delete()
+    return Response("Task deleted successfully.")
