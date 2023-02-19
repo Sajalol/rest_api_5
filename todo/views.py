813a555da29cc30f1f123f5ba251207ec10c5912
Stian Lystad <Stianlystad_1@hotmail.com>
2023-02-19 14:21:14 +0000

    added permissions_classes to taskupdate

--- todo/views.py
+++ todo/views.py
@@ -10,7 +10,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import serializers
 from django.shortcuts import get_object_or_404
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 """
