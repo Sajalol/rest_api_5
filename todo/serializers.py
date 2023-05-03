ad3f20e14212db69c0fa0e1d4e1a6fb2584a9026
Stian Lystad <Stianlystad_1@hotmail.com>
2023-05-03 12:16:04 +0000

    added 'is_staff' to serializers

--- todo/serializers.py
+++ todo/serializers.py
@@ -12,7 +12,7 @@ class TaskSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_staff']
 
 
 
