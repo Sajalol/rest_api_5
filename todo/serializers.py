326307be8aa55560691d12eb7414f142058d654d
Stian Lystad <Stianlystad_1@hotmail.com>
2023-05-05 21:23:59 +0000

    error password handling in frontend

--- todo/serializers.py
+++ todo/serializers.py
@@ -21,3 +21,10 @@ class UserCreateSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'email', 'password', 'first_name', 'last_name']
         extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
